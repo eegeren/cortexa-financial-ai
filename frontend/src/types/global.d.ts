e9ed48acd4f7e13d31e10fd664a7c1b6b13b2114
Yusufege Eren <yusufegeeren@192.168.1.4>
2025-09-27 16:56:45 +0300

    upd.

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -1,7 +1,7 @@
 export {};
 
 declare global {
-  class Worker {
+  declare class Worker {
     constructor(stringUrl: string | URL, options?: { type?: 'classic' | 'module'; credentials?: 'omit' | 'same-origin' | 'include'; name?: string });
     postMessage(message: unknown, transfer?: unknown[]): void;
     terminate(): void;
